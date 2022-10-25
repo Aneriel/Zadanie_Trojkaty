d4920ef4c0fd89c1e504c7eeb19df1e1b2e1fbb4
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-10-25 10:19:14 +0200

    #01-Added_Rounding

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Podaj pierwszy bok trójkąta:");
+﻿Console.Write("Podaj pierwszy bok trójkąta:");
 double bokPierwszy = double.Parse(Console.ReadLine());
 Console.Write("Podaj drugi bok trójkąta:");
 double bokDrugi = double.Parse(Console.ReadLine());
 Console.Write("Podaj trzeci bok trójkąta:");
 double bokTrzeci = double.Parse(Console.ReadLine());
 double obwodTrojkataDziel = (bokPierwszy + bokDrugi + bokTrzeci)/2;
+double pole = Math.Round(Math.Sqrt(obwodTrojkataDziel * (obwodTrojkataDziel - bokPierwszy)*(obwodTrojkataDziel - bokDrugi)*(obwodTrojkataDziel - bokTrzeci)),2 );
+
+
 if (bokPierwszy > bokDrugi + bokTrzeci|| bokDrugi > bokPierwszy + bokTrzeci || bokTrzeci > bokDrugi + bokPierwszy)
 {
     Console.Write("Błędne dane. Trójkąta nie można zbudować!");
@@ -13,7 +15,7 @@ if (bokPierwszy > bokDrugi + bokTrzeci|| bokDrugi > bokPierwszy + bokTrzeci || b
 else
 {
     Console.WriteLine($"obwód = {bokPierwszy + bokDrugi + bokTrzeci}");
-    Console.WriteLine($"pole ={Math.Sqrt(obwodTrojkataDziel * (obwodTrojkataDziel - bokPierwszy)*(obwodTrojkataDziel - bokDrugi)*(obwodTrojkataDziel - bokTrzeci) )} ");
+    Console.WriteLine($"pole ={pole} ");
     Console.WriteLine($"Test testu");
 
     if (bokTrzeci == bokPierwszy && bokDrugi == bokPierwszy)
